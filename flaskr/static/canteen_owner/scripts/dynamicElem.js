a7f9a083bbcd6d640635d439731f229898410d11
BhargaviV <bhargavivittalapuram@gmail.com>
2018-10-01 19:10:26 +0530

    Added js part

--- flaskr/static/canteen_owner/scripts/dynamicElem.js
+++ flaskr/static/canteen_owner/scripts/dynamicElem.js
@@ -1,4 +1,17 @@
-$("this").click(function()
+/*$(".panel").click(function(e)
 {
-    alert("Clicker");
-})
+        alert("Collapse");
+        var $_target =  $(e.currentTarget);
+        var $_panelBody = $_target.find(".panel");
+        if($_panelBody)
+        {
+          $_panelBody.collapse('toggle');
+        }
+});*/
+
+$("#AddItem").click(function()
+{
+    var content= $('.panel').html();
+    $(".col-md-7").append("<div class='panel'>"+content+"</div>"); 
+    $('.panel').collapse({ parent: true, toggle: true });
+}) ;
