11924fb22438900df81adb0da369b3f0d1391666
ganesh <ganesh3597@gmail.com>
2018-10-02 13:22:03 +0530

    Fix templating | TBD on upcoming branch

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template
 import db
 app = Flask(__name__)
 
+@app.route('/elements.html')
+def elements():
+	return render_template('canteen_owner/elements.html')
+
+@app.route('/owner_main')
+def owner_index():
+	return render_template('canteen_owner/index.html')
+
 @app.route('/items')
-def index():
+def items_index():
 	#table_name = 'Items'
 	dbase = db.get_conn('data/canteen.db')
 	return render_template('canteen-items/index.html', data = db.get_items('Items', dbase))
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
