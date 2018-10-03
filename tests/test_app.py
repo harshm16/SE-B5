8fe91b8ac9f9bda6d9c9e8eee556e9f78eab3e7c
Ganesh K <ganesh3597@gmail.com>
2018-10-03 18:54:17 +0530

    Canteen-Side Tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,46 @@ import pytest_flask
 from flask import url_for
 
 class TestApp:
-    def test_index(self, client):
-        res = client.get(url_for('owner_index'))
-        assert res.status_code == 200
+	def test_index(self, client):
+		res = client.get(url_for('owner_index'))
+		assert res.status_code == 200
+	
+	def test_elements(self, client):
+		res = client.get(url_for('elements'))
+		assert res.status_code == 200
+	
+	def test_panels(self, client):
+		res = client.get(url_for('panels'))
+		assert res.status_code == 200
+		
+	def test_page_profile(self, client):
+		res = client.get(url_for('page_profile'))
+		assert res.status_code == 200
+		
+	def test_page_login(self, client):
+		res = client.get(url_for('page_login'))
+		assert res.status_code == 200
+		
+	def test_page_lockscreen(self, client):
+		res = client.get(url_for('page_lockscreen'))
+		assert res.status_code == 200
+		
+	def test_charts(self, client):
+		res = client.get(url_for('charts'))
+		assert res.status_code == 200
+		
+	def test_notifications(self, client):
+		res = client.get(url_for('notifications'))
+		assert res.status_code == 200
+		
+	def test_tables(self, client):
+		res = client.get(url_for('tables'))
+		assert res.status_code == 200
+		
+	def test_icons(self, client):
+		res = client.get(url_for('icons'))
+		assert res.status_code == 200
+	
+	def test_typography(self, client):
+		res = client.get(url_for('typography'))
+		assert res.status_code == 200
