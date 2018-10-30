4125e1bd290fe87b214d3257a66bc1454aa22e5e
ganesh <ganesh3597@gmail.com>
2018-10-30 16:48:34 +0530

    Util functions

--- flaskr/db_utils.py
+++ flaskr/db_utils.py
@@ -37,3 +37,17 @@ def get_canteen_details(db_name):
 	cursor = conn.cursor(dictionary=True)
 	cursor.execute('select Canteen_id,Canteen_name,Owner_name from Canteen, Owner where Canteen.Owner_id=Owner.Owner_id')
 	return cursor.fetchall()
+
+def get_item_user_details(db_name):
+	conn = mysql.connector.connect(
+				host="localhost",
+				user="root",
+				passwd="",
+				database=db_name
+			)
+	cursor = conn.cursor(dictionary=True)
+	
+	items = get_items('Items', db_name)
+	for item in items:
+		cursor.execute('select * from Users where User_id in (select User_id from Purchases where Item_id = %s)' % item['Items_id'])
+		print(cursor.fetchall())
